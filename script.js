fa30d866a438ac6534b7ca0305e485393e326121
eduappdev <140945057+eduappdev@users.noreply.github.com>
2023-07-30 06:35:04 -0300

    index

---
+++ script.js
@@ -0,0 +1,28 @@
+var id1=document.getElementById('d1')
+ 
+ class Calendario{
+   
+
+    constructor(){
+        this.id =0;
+        this.dia=0;
+        this.mes=0;
+        this.ano =0;
+        this.tano=0;
+
+
+
+
+
+    }
+    
+    adicionar(){
+       id1.textContent=""
+    } 
+
+    deletar(){
+        alert("foi deletado")
+    }
+
+}
+var calendario = new Calendario()
